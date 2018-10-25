70f6d866ee4fc66677e74bff6eee54844aa5facc
Alan Tsai <alan@alantsai.net>
2018-10-25 21:33:02 +0800

    加入RoomReservation POCO

---
+++ src/MHAT.HotelBotV4/Model/RoomReservation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MHAT.HotelBotV4.Model
+{
+    public class RoomReservation
+    {
+        public DateTime StartDate { get; set; }
+        public int NumberOfNightToStay { get; set; }
+        public int NumberOfPepole { get; set; }
+        public string BedSize { get; set; }
+    }
+}
