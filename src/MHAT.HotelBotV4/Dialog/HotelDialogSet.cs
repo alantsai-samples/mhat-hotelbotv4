8eaf4480efbea8868da327538656bbf3e9465ff6
Alan Tsai <alan@alantsai.net>
2018-10-28 17:08:04 +0800

    加入要繼承的dialogset

---
+++ src/MHAT.HotelBotV4/Dialog/HotelDialogSet.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MHAT.HotelBotV4.Dialog
+{
+    public class HotelDialogSet : DialogSet
+    {
+        public HotelDialogSet(IStatePropertyAccessor<DialogState> dialogState) 
+            : base(dialogState)
+        {
+        }
+    }
+}
